64d6b53edd4b2b4bf1673bff5af08e0c3260de77
Buridius <D_Jackstien@web.de>
2026-01-12 11:48:08 +0100

    Styling

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -12,29 +12,40 @@ export default function LoginPage(props:Readonly<any>) {
 
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-    console.log("Ignore this" + props, email, password);
+
     return (
-        <>
-            <div>Login Page</div>
+        <div className="p-6">
+            <h2 className="text-xl font-semibold text-purple-800 space-x-6 mb-6">Login Page</h2>
             <form onSubmit={doLogin}>
-                <label>E-mail:
-                    <input onChange={
+                <label className="text-purple-900 font-bold mr-2">
+                    E-mail:
+                </label>
+                    <input
+                        onChange={
                         (e) =>
                             setEmail(e.target.value)}
-                    placeholder="Enter your email"
+                        placeholder="Enter your email"
+                        className="min-w-[200px] border border-gray-300 px-3 py-2 rounded-md focus:ring-2 focus:ring-purple-500 focus:border-purple-500 mr-4"
                     />
-                </label>
 
-                <label>Password:
-                    <input onChange={
+                <label className="text-purple-900 font-bold mr-2">
+                    Password:
+                </label>
+                    <input
+                        onChange={
                         (e) =>
                             setPassword(e.target.value)}
-                    placeholder="Enter your password"
-                    type="password"
+                        placeholder="Enter your password"
+                        type="password"
+                        className="min-w-[200px] border border-gray-300 px-3 py-2 rounded-md focus:ring-2 focus:ring-purple-500 focus:border-purple-500 mr-4"
                     />
-                </label>
+                <button
+                    onClick={doLogin}
+                    className="px-6 py-2 bg-purple-700 text-white font-semibold rounded-md hover:bg-purple-800"
+                >
+                    Login
+                </button>
             </form>
-            <button onClick={doLogin}>Login</button>
-        </>
+        </div>
     )
 }
--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -2,6 +2,8 @@ export default function () {
     return (
         <>
             Settings!
+            Any ideas for settings?
+            I have no clue so far.
         </>
     )
 }
