6011d0e689f2ca5f145c2c074f533370cb2a451e
Buridius <D_Jackstien@web.de>
2026-02-16 18:45:56 +0100

    Error connecting fixed

--- backend/src/main/java/org/example/edhtrack/controller/RootController.java
+++ backend/src/main/java/org/example/edhtrack/controller/RootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    @GetMapping(value = {"/", "/{x:[^\\.]*}", "/**/{x:[^\\.]*}"})
+    @GetMapping(value = { "/", "/{path:[^\\.]*}" })
     public String forward() {
         return "forward:/index.html";
     }
