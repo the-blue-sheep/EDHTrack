0ae3bfe72676f659a84cb8cdc26762c248681ef6
Buridius <D_Jackstien@web.de>
2026-02-17 02:37:21 +0100

    SonarQube suggestions implemented

--- backend/src/main/java/org/example/edhtrack/controller/RootController.java
+++ backend/src/main/java/org/example/edhtrack/controller/RootController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class RootController {
 
-    @GetMapping(value = { "/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}" })
+    @GetMapping(value = { "/", "/{path:[^\\.]*}" })
     public String forward(@PathVariable(required = false) String path) {
         return "forward:/index.html";
     }
