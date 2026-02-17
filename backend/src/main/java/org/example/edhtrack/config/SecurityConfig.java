62d5aa2dcbe060b6cc60c541951fd0f3a1111320
Buridius <D_Jackstien@web.de>
2026-02-17 02:34:11 +0100

    SonarQube suggestions implemented

--- backend/src/main/java/org/example/edhtrack/config/SecurityConfig.java
+++ backend/src/main/java/org/example/edhtrack/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.example.edhtrack.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
