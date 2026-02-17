73427dd65ece77aeab09bbf0d52c0dc2724416ac
Buridius <D_Jackstien@web.de>
2026-02-17 03:24:54 +0100

    swagger implemented

--- backend/src/main/java/org/example/edhtrack/controller/RootController.java
+++ backend/src/main/java/org/example/edhtrack/controller/RootController.java
@@ -9,6 +9,13 @@ public class RootController {
 
     @GetMapping(value = { "/", "/{path:[^\\.]*}" })
     public String forward(@PathVariable(required = false) String path) {
+        if (path == null || path.isEmpty() ||
+                path.startsWith("api") ||
+                path.startsWith("v3") ||
+                path.startsWith("swagger-ui")) {
+            if (path == null) return "forward:/index.html";
+            return "forward:/" + path;
+        }
         return "forward:/index.html";
     }
 }
